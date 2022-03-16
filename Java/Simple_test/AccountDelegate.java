a8d9591ccda33ae129edd9c8120327520ad64f67
andka428-andka <62101420+andka428-andka@users.noreply.github.com>
2022-03-16 09:39:32 +0100

    Add files via upload

--- Java/Simple_test/AccountDelegate.java
+++ Java/Simple_test/AccountDelegate.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import com.cbe.*;
 
 public class AccountDelegate extends AccountEventProtocol {
@@ -9,7 +8,13 @@ public class AccountDelegate extends AccountEventProtocol {
     
     @Override
     public void onLogin(long atState, CloudBackend cloudbackend) {
-        System.out.println("You've reached onlogin in Java congrats " + cloudbackend.account().username() + " " + cloudbackend.account().rootContainer().id());
+        System.out.println("onlogin: " + cloudbackend.account().username() + " " + cloudbackend.account().rootContainer().id());
+        this.finished = true;
+    }
+
+    @Override
+    public void onError(long failedAtState, long code, String reason, String message) {
+        System.out.println("Login error: code=" + code + ", reson=\"" + reason + "\", message=\"" + message + "\"");
         this.finished = true;
     }
 }
