153bb84bf108a366c3e9dfd37d8dc91b8d0b45d3
someawe <dev@weister.net>
2022-04-08 16:45:23 +0200

    lib for Java

--- Java/lib/install.sh
+++ Java/lib/install.sh
@@ -1,7 +1,6 @@
-jar xf libcbe.jar 
-
-sudo cp lib* /usr/lib 
-
-shopt -s extglob
-rm *.!(txt|jar|sh)
-
+#!/usr/bin/bash
+unzip libcbeJava.zip 
+echo
+echo "To ease finding the SDK, you may set it in an environment variable, e.g.:"
+echo export 'CBE_JAVA_SDK='`pwd`
+echo 'echo ${CBE_JAVA_SDK}'
