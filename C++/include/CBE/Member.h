47bb677549cd0d35db068a4f34e081d568fb875c
andka428-andka <62101420+andka428-andka@users.noreply.github.com>
2022-05-02 11:42:20 +0200

    Add files via upload

---
+++ C++/include/CBE/Member.h
@@ -0,0 +1,44 @@
+#ifndef CBE_MEMBER_H
+#define CBE_MEMBER_H
+
+#include "CBE/Types.h"
+
+namespace CBE {
+    
+  struct Request {
+      uint64_t userId{0};
+      std::string alias{""};
+      std::string applicationComment{""};
+      uint64_t date{0}; //unix time
+  };
+
+//this might be better to have just as a string on the member. also if so then list members should call list roles.
+  struct Role {
+    std::string role;
+  };
+
+  class Member {
+    public:
+
+    virtual void kick(std::string kickComment, CBE::GroupDelegatePtr delegate);
+    virtual void ban(std::string banComment, CBE::GroupDelegatePtr delegate);
+    virtual void unBan(CBE::GroupDelegatePtr delegate);
+    virtual uint64_t memberId() const;
+
+    /** Returns the member name.*/
+    virtual std::string name() const;
+    virtual CBE::Visibility visibility() const;
+    virtual CBE::group_id_t groupId() const;
+    virtual std::map<std::pair<uint64_t,uint64_t>,std::pair<uint64_t,std::string>> getMemberBanInfo();
+ 
+    virtual ~Member() = default;
+
+    protected:
+    Member() {}
+
+  };
+
+
+}
+
+#endif //CBE_MEMBER_H
