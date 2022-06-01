266655c8951b48f632acf03b761f85d2a06374a8
andka428-andka <62101420+andka428-andka@users.noreply.github.com>
2022-06-01 15:11:08 +0200

    Update LoadTable.java

--- Java/Examples/LoadTables/LoadTable.java
+++ Java/Examples/LoadTables/LoadTable.java
@@ -114,7 +114,7 @@ public class LoadTable {
       // load a properties file
       prop.load(input);
 
-      TransferDelegate delegate = new TransferDelegate();
+      TransferUploadDelegate delegate = new TransferUploadDelegate();
       byte[] data = Files.readAllBytes(Paths.get(prop.getProperty("binaryPath")));
       tempObj = container.uploadBinary(prop.getProperty("binaryFileName"), data, delegate);
       System.out.println("tempObject id:" + tempObj.id());
@@ -147,7 +147,7 @@ public class LoadTable {
       // load a properties file
       prop.load(input);
 
-      TransferDelegate delegate = new TransferDelegate();
+      TransferUploadDelegate delegate = new TransferUploadDelegate();
       tempObj = container.upload(prop.getProperty("filename"), prop.getProperty("path") , delegate);
       System.out.println("tempObject id:" + tempObj.id());    
 
