9178632c1af2b4ebc82f2d656a67b44dc0033448
someawe <dev@weister.net>
2023-02-10 13:24:20 +0100

    2.0.2

--- C++/Examples/Airplanes/Airplanes.cpp
+++ C++/Examples/Airplanes/Airplanes.cpp
@@ -1,7 +1,5 @@
 #include "Airplanes.h"
 
-// #include "QueryDelegate.h"
-
 #include "cbe/CloudBackend.h"
 #include "cbe/QueryChain.h"
 
@@ -9,49 +7,52 @@
 #include <condition_variable>
 #include <iomanip>
 #include <mutex>
-#include <stdexcept>
 #include <sstream>
+#include <stdexcept>
 
+#include "MyDelegates.cpp"
+#include "user_credentials.cpp"
 
-Airplanes::Airplanes(const std::string& credentials) : 
-                                               cloudBackend{cbe::DefaultCtor{}}, 
-                                               account{cbe::DefaultCtor{}} {
-  constexpr const auto  username = "githubtester2";
-  constexpr const auto  password = "gitHubTester2password";
-  constexpr const auto  tenant   = "cbe_githubtesters";
-  
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+Airplanes::Airplanes() : cloudBackend{cbe::DefaultCtor{}}, 
+                         account{cbe::DefaultCtor{}} {
+
   std::shared_ptr<LogInDelegate> logInDelegate = 
-                               std::make_shared<LogInDelegate>();
+                                              std::make_shared<LogInDelegate>();
 
   logInDelegate->cloudBackend = cbe::CloudBackend::logIn(username, 
                                                          password, 
                                                          tenant, 
                                                          logInDelegate);
   logInDelegate->waitForRsp();
+
   cloudBackend = logInDelegate->cloudBackend;
   account = cloudBackend.account();
+  if (logInDelegate->errorInfo) {
+    std::cout << "Error info: " << std::endl;
+    std::cout << logInDelegate->errorInfo << std::endl;
+  }
 } // Airplanes ctor
 
-namespace {
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-
-} // Anonymous namespace
-
-cbe::QueryResult Airplanes::query(cbe::Filter         filter,
+cbe::QueryResult Airplanes::query(cbe::Filter      filter,
                                   cbe::ContainerId containerId) {
   
   std::shared_ptr<QueryDelegate> queryDelegate = 
                                               std::make_shared<QueryDelegate>();
   cloudBackend.query(containerId, filter, queryDelegate);
   queryDelegate->waitForRsp();
-
+  if (queryDelegate->errorInfo) {
+    std::cout << "Error info: " << std::endl;
+    std::cout << queryDelegate->errorInfo << std::endl;
+    throw std::runtime_error("airport query");
+  }
   return queryDelegate->queryResult;
 }
 
-namespace {
-
-
-} // Anonymous namespace
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 cbe::Container Airplanes::createContainer(cbe::Container     parentContainer,
                                           const std::string& name) {
@@ -60,13 +61,22 @@ cbe::Container Airplanes::createContainer(cbe::Container     parentContainer,
 
   parentContainer.createContainer(name, createContainerDelegate);
   createContainerDelegate->waitForRsp();
+  if (createContainerDelegate->errorInfo) {
+    std::cout << "Error info: " << std::endl;
+    std::cout << createContainerDelegate->errorInfo << std::endl;
+    throw std::runtime_error(name+" container create");
+  }
   return createContainerDelegate->container; 
 } 
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 cbe::Object Airplanes::createObject(cbe::Container     parentContainer,
-                                       const std::string&    name,
-                                       cbe::KeyValues&&  keyValues) {
- 
+                                    const std::string& name,
+                                    cbe::KeyValues&&   keyValues) {
+  std::cout << std::setw(14) << name;
+  std::shared_ptr<QueryDelegate> queryDelegate = 
+                                              std::make_shared<QueryDelegate>();
   std::for_each(std::begin(keyValues), std::end(keyValues),
        [](const cbe::KeyValues::value_type& entry) {
          std::cout << std::setw(14) << entry.second.first;
@@ -76,14 +86,29 @@ cbe::Object Airplanes::createObject(cbe::Container     parentContainer,
                                      std::make_shared<MyCreateObjectDelegate>();
   parentContainer.createObject(name, createObjectDelegate, keyValues);
   createObjectDelegate->waitForRsp();
+  if (createObjectDelegate->errorInfo) {
+    std::cout << "Error info: " << std::endl;
+    std::cout << createObjectDelegate->errorInfo << std::endl;
+    throw std::runtime_error(name+" object create");
+  }
 
   // std::cout << "Created object \"" << createObjectDelegate->object.name() 
   //           << "\" with id " << createObjectDelegate->object.id() << '\n';
   return createObjectDelegate->object;
 }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 void Airplanes::removeContainer(cbe::Container container) {
   std::shared_ptr<RemoveDelegate> removeDelegate = 
-                                  std::make_shared<RemoveDelegate>();
+                                             std::make_shared<RemoveDelegate>();
   container.remove(removeDelegate);
   removeDelegate->waitForRsp();
+  if (removeDelegate->errorInfo) {
+    std::cout << "Error info: " << std::endl;
+    std::cout << removeDelegate->errorInfo << std::endl;
+    throw std::runtime_error(container.name()+" container delete");
+  }
 }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- C++/Examples/Airplanes/compile.sh
+++ C++/Examples/Airplanes/compile.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/sh
 # compile.sh <excercise>
-# release 2022-07-19
+# release 2023-02-10
 
 PARENTSCRIPT_PATH="$(dirname "$0")"
-echo "$PARENTSCRIPT_PATH" 
-cd "$PARENTSCRIPT_PATH"
-echo $PWD
+cd ${PARENTSCRIPT_PATH}
+echo ${PWD}
 
 if [ $# -gt 0 ]
 then
@@ -17,22 +17,20 @@ echo "computer architechture ${ARCH}"
 case "$ARCH" in
     "x86_64")
     COMPILER_COMMAND="g++ -std=c++11 -pthread "
-    echo $PWD
     # libCBE="../../lib/Linux_x86/libcb_sdk.so"
     libCBE="../../lib/Linux_x86/libcb_sdk.a"
     CODE_PATH="./"
-    echo $CODE_PATH
     ;;
 
-    "armv6l")
-    COMPILER_COMMAND="arm-linux-gnueabihf-g++ -mcpu=arm1176jzf-s -mfpu=vfp -fPIC -ldl -pthread "
-    libCBE="libCloudBackend-rpi.a"
-    ;;
+    # "armv6l")
+    # COMPILER_COMMAND="arm-linux-gnueabihf-g++ -mcpu=arm1176jzf-s -mfpu=vfp -fPIC -ldl -pthread "
+    # libCBE="libCloudBackend-rpi.a"
+    # ;;
 
-    "armv7l")
-    COMPILER_COMMAND="arm-linux-gnueabihf-g++ -mcpu=cortex-a53 -mfpu=neon-vfpv4 -fPIC -ldl -pthread "
-    libCBE="libCloudBackend-rpi.a"
-    ;;
+    # "armv7l")
+    # COMPILER_COMMAND="arm-linux-gnueabihf-g++ -mcpu=cortex-a53 -mfpu=neon-vfpv4 -fPIC -ldl -pthread "
+    # libCBE="libCloudBackend-rpi.a"
+    # ;;
 
     *)
     uname -a
@@ -43,12 +41,13 @@ esac
 
 case "$exercise" in
   "1")
-  # Section 1:
     echo "compile example code."
      ${COMPILER_COMMAND} -o "airplanes" "${CODE_PATH}main.cpp" "${CODE_PATH}Airplanes.cpp" ${libCBE} -I "../../include"
     if [ $? -eq 0 ]
     then
         echo "to run use: sh run.sh"
+    else
+        echo "Error encountered."
     fi
     ;;
 
