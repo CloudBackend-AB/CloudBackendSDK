f6d0cfd2d02c8245616743476a138a49df56975a
CloudBackend-AB <73992900+CloudBackend-AB@users.noreply.github.com>
2023-09-19 11:16:35 +0200

    Release 2.1.1
    Updated lib

--- C++/installC++SDK.sh
+++ C++/installC++SDK.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # installC++SDK.sh
-# CloudBackend 2023-09-15.
+# CloudBackend 2023-09-19.
 echo "Installing CloudBackend C++ SDK."
 echo -n "Checking g++ version: "
 g++ --version                               # check that the compiler is installed
@@ -10,7 +10,7 @@ then
   sudo apt-get install g++                  # if not already installed
 fi
 cd lib/Linux_x86
-unzip libcd_sdk.zip
+unzip lib*.zip
 cd -
 echo
 echo "Compile and run C++ installation test"
